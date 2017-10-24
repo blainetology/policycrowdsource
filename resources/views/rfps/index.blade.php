e7e39a879e556df614718036673f1c4b5aec2865
Blaine Jones <blainecjones@gmail.com>
2017-10-24 01:51:47 -0700

    rfp show layout

--- resources/views/rfps/index.blade.php
+++ resources/views/rfps/index.blade.php
@@ -3,18 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1>Requests for Policies</h1>
-                </div>
-            </div>
-            <div class="row">
-                @foreach($rfps as $rfp)
-                    @include('partials.rfp-box',['rfp'=>$rfp])
-                @endforeach
-            </div>
+        <div class="col-lg-12">
+            <h1>Requests for Policies</h1>
         </div>
+    </div>
+    <div class="row">
+        @foreach($rfps as $rfp)
+            @include('partials.rfp-box',['rfp'=>$rfp])
+        @endforeach
     </div>
 </div>
 @endsection
--- resources/views/rfps/show.blade.php
+++ resources/views/rfps/show.blade.php
@@ -4,43 +4,48 @@
 <br/>
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-md-9">
-                    <h1>{{$rfp->name}}</h1>
-                    <p class="small">
-                    <strong>Prepared and Submitted By:</strong> 
-                    @foreach($rfp->collaborators as $cindex=>$collaborator)
-                        @if($cindex<$rfp->collaborators->count()-1 && $rfp->collaborators->count()>2)
-                            {{$collaborator->user->full_name()}},  
-                        @elseif($cindex<$rfp->collaborators->count()-1)
-                            {{$collaborator->user->full_name()}}  
-                        @else 
-                            &amp; {{$collaborator->user->full_name()}} 
-                        @endif
-                    @endforeach
-                    </p>
-                    <p>{!!$rfp->full_details!!}</p>
+        <div class="col-md-9">
+            <h1>{{$rfp->name}}<br/><small>{!!$rfp->short_overview!!}</small></h1>
+            <p class="small">
+            <strong>Prepared and Submitted By:</strong> 
+            @foreach($rfp->collaborators as $cindex=>$collaborator)
+                @if($cindex<$rfp->collaborators->count()-1 && $rfp->collaborators->count()>2)
+                    {{$collaborator->user->full_name()}},  
+                @elseif($cindex<$rfp->collaborators->count()-1)
+                    {{$collaborator->user->full_name()}}  
+                @else 
+                    &amp; {{$collaborator->user->full_name()}} 
+                @endif
+            @endforeach
+            </p>
+        </div>
+        <div class="col-md-3">
+            <div class="well well-sm">
+                <span class="pull-left policy-rating rating_{{round($rfp->rating)}}"> {{number_format($rfp->rating_count,0)}} Votes </span>
+                <div class="rating-box policy-rating pull-right text-right" id="ratingBoxRfp{{$rfp->id}}">
+                @if(\Auth::check())
+                    <a href="javascript:rate_ajax({{$rfp->id}},null,-2)" title="strongly do not support" class="rating-thumb rating-2"><i class="fa fa-thumbs-down" aria-hidden="true"></i></a> 
+                    <a href="javascript:rate_ajax({{$rfp->id}},null,-1)" title="do not support" class="rating-thumb rating-1"><i class="fa fa-thumbs-down" aria-hidden="true"></i></a> 
+                    <a href="javascript:rate_ajax({{$rfp->id}},null,1)" title="support" class="rating-thumb rating1"><i class="fa fa-thumbs-up" aria-hidden="true"></i></a> 
+                    <a href="javascript:rate_ajax({{$rfp->id}},null,2)" title="strongly support" class="rating-thumb rating2"><i class="fa fa-thumbs-up" aria-hidden="true"></i></a>
+                @else
+                    <a href="javascript:showLoginModal()" class="btn btn-xs btn-default">Login to Rate</a>
+                @endif
                 </div>
-                <div class="col-md-3">
-                    <span class="pull-left policy-rating rating_{{round($rfp->rating)}}"> {{number_format($rfp->rating_count,0)}} Votes </span>
-                    <div class="rating-box policy-rating pull-right text-right" id="ratingBoxRfp{{$rfp->id}}">
-                    @if(\Auth::check())
-                        <a href="javascript:rate_ajax({{$rfp->id}},null,-2)" title="strongly do not support" class="rating-thumb rating-2"><i class="fa fa-thumbs-down" aria-hidden="true"></i></a> 
-                        <a href="javascript:rate_ajax({{$rfp->id}},null,-1)" title="do not support" class="rating-thumb rating-1"><i class="fa fa-thumbs-down" aria-hidden="true"></i></a> 
-                        <a href="javascript:rate_ajax({{$rfp->id}},null,1)" title="support" class="rating-thumb rating1"><i class="fa fa-thumbs-up" aria-hidden="true"></i></a> 
-                        <a href="javascript:rate_ajax({{$rfp->id}},null,2)" title="strongly support" class="rating-thumb rating2"><i class="fa fa-thumbs-up" aria-hidden="true"></i></a>
-                    @else
-                        <a href="javascript:showLoginModal()" class="btn btn-xs btn-default">Login to Rate</a>
-                    @endif
-                    </div>
-                    <br clear="both"/>
+                <br clear="both"/>
+                <div style="padding:6px 0 0;">
                     {!!\App\Rating::getPolicyThumbs($rfp)!!}
                 </div>
             </div>
-            <br/>
         </div>
     </div>
+    <hr class="clearfix" />
+    <div class="row">
+        <div class="col-md-12">
+            <p>{!!$rfp->full_details!!}</p>
+        </div>
+    </div>
+    <br/>
     @if($rfp->policies->count()>0)
         <div class="row">
             <div class="col-lg-12"> 
